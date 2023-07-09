34a1974e8f70aa23e2fffa356a6b5ac88005491e
Ondrej89 <ondrejdudas89@gmail.com>
2023-07-09 15:19:33 +0200

    ...

---
+++ app/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <section className="flexStart footer">
+         <div className="flexBetween footer_copyright text-center mb-3">
+            <p>@ 2023  designed by LoneWolf. </p>
+            {/* <p className="text-gray">
+                <span className="text-black font-semibold">4</span> projects submitted
+            </p> */}
+        </div>
+    </section>
+  )
+}
+
+export default Footer
